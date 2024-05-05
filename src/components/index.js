25444c67e0e7f5157b8b37a377ea37c17dfe094c
Himasnhu <himanshubhartimax@gmail.com>
2024-05-05 21:14:30 +0530

    create login and signup components

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,12 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import Select from "./Select"
+
+
 
 
 export {
@@ -10,5 +16,9 @@ export {
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select
 }
