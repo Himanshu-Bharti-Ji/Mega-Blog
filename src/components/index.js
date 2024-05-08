6d2ecd49a436e05ebdf07b46daaa1e3a45936c8d
Himasnhu <himanshubhartimax@gmail.com>
2024-05-08 18:02:18 +0530

    Add Pages and routing

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,11 @@ import Input from "./Input"
 import PostCard from "./PostCard"
 import Select from "./Select"
 import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout"
 
 
 
@@ -22,5 +27,10 @@ export {
     Input,
     PostCard,
     Select,
-    RTE
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
